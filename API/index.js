e4f0508e2525bdaf71b354576f0f79235f34c31b
Tatana <you@example.com>
2020-10-15 23:05:44 +0300

    init 05-images

--- API/index.js
+++ API/index.js
@@ -3,12 +3,21 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const contactsRouter = require('./contacts/contacts.router');
+const userRouter = require('./users/users.router');
 
 require('dotenv').config();
 
-class ContacsServer {
+module.exports = class ContacsServer {
   constructor() {
     this.server = null;
+  }
+
+  async start() {
+    this.initServer();
+    this.initMiddlewares();
+    this.initRoutes();
+    await this.initDatabase();
+    return this.startListening();
   }
 
   initServer() {
@@ -17,49 +26,28 @@ class ContacsServer {
 
   initMiddlewares() {
     this.server.use(express.json());
+    this.server.use(express.static('public'));
     this.server.use(morgan('tiny'));
     this.server.use(cors());
   }
 
   initRoutes() {
     this.server.use('/contacts', contactsRouter);
+    this.server.use('/', userRouter);
   }
 
   async initDatabase() {
-    try {
-      await mongoose.connect(
-        process.env.MONGODB_URL,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
-        err => {
-          if (err) {
-            console.log(err);
-            return process.exit(1);
-          }
-          console.log('Database connection successful');
-        },
-      );
-    } catch (err) {
-      console.log(err);
-    }
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   }
 
   startListening() {
     const PORT = process.env.PORT;
-    this.server.listen(PORT, () => {
-      console.log('Server started listening on port', PORT);
+
+    return this.server.listen(PORT, () => {
+      console.log('Server listening on port', PORT);
     });
   }
-
-  async start() {
-    this.initServer();
-    this.initMiddlewares();
-    this.initRoutes();
-    await this.initDatabase();
-    this.startListening();
-  }
-}
-
-new ContacsServer().start();
+};
