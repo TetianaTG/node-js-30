fb4719f3243549c95f517b03060a94d434d23111
TetianaTG <53144889+TetianaTG@users.noreply.github.com>
2020-10-26 19:50:58 +0200

    Update index.js

--- API/index.js
+++ API/index.js
@@ -3,21 +3,12 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const contactsRouter = require('./contacts/contacts.router');
-const userRouter = require('./users/users.router');
 
 require('dotenv').config();
 
-module.exports = class ContacsServer {
+class ContacsServer {
   constructor() {
     this.server = null;
-  }
-
-  async start() {
-    this.initServer();
-    this.initMiddlewares();
-    this.initRoutes();
-    await this.initDatabase();
-    return this.startListening();
   }
 
   initServer() {
@@ -26,28 +17,50 @@ module.exports = class ContacsServer {
 
   initMiddlewares() {
     this.server.use(express.json());
-    this.server.use(express.static('public'));
     this.server.use(morgan('tiny'));
     this.server.use(cors());
   }
 
   initRoutes() {
     this.server.use('/contacts', contactsRouter);
-    this.server.use('/', userRouter);
   }
 
   async initDatabase() {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    try {
+      await mongoose.connect(
+          process.env.MONGODB_URL,
+          {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          },
+          err => {
+            if (err) {
+              console.log(err);
+              return process.exit(1);
+            }
+            console.log('Database connection successful');
+          },
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   startListening() {
     const PORT = process.env.PORT;
-
-    return this.server.listen(PORT, () => {
-      console.log('Server listening on port', PORT);
+    this.server.listen(PORT, () => {
+      console.log('Server started listening on port', PORT);
     });
   }
-};
+
+  async start() {
+    this.initServer();
+    this.initMiddlewares();
+    this.initRoutes();
+    await this.initDatabase();
+    this.startListening();
+  }
+}
+
+new ContacsServer().start();
+
