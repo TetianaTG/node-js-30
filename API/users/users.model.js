5fe77f8c7f71bed09cdab085f283e655b858410f
TetianaTG <53144889+TetianaTG@users.noreply.github.com>
2020-10-28 12:25:04 +0200

    Update users.model.js

--- API/users/users.model.js
+++ API/users/users.model.js
@@ -2,21 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema(
-    {
-        email: String,
-        password: String,
-        avatarURL: String,
-        subscription: {
-            type: String,
-            enum: ['free', 'pro', 'premium'],
-            default: 'free',
-        },
-        token: String,
-        verificationToken: String,
+  {
+    email: String,
+    password: String,
+    avatarURL: String,
+    subscription: {
+      type: String,
+      enum: ['free', 'pro', 'premium'],
+      default: 'free',
     },
-    {
-        versionKey: false,
-    },
+    token: String,
+    verificationToken: String,
+  },
+  {
+    versionKey: false,
+  },
 );
 
 const usersModel = mongoose.model('Users', usersSchema, 'users');
