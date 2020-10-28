241cbd28adb2c4642b6d8be1f365d701f5bd3c8e
TetianaTG <53144889+TetianaTG@users.noreply.github.com>
2020-10-28 12:47:08 +0200

    Update users.controller.js

--- API/users/users.controller.js
+++ API/users/users.controller.js
@@ -1,39 +1,21 @@
-require('dotenv').config();
 const fs = require('fs');
 const Joi = require('@hapi/joi');
-const bcrypt = require('bcrypt');
 const usersModel = require('./users.model');
 const Avatar = require('avatar-builder');
-const jwt = require('jsonwebtoken');
-const val = require('../validation/validation');
 const { promises: fsPromises } = fs;
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
+require('dotenv').config();
+
 const Subtitle = ['free', 'pro', 'premium'];
 
 class UsersController {
-  validateAddUser(req, res, next) {
-    const schema = Joi.object(val.createValidation);
-    const validation = schema.validate(req.body);
-
-    if (validation.error) return handleValidationError(res, validation);
-
-    next();
-  }
-  validateLoginUser(req, res, next) {
-    const schema = Joi.object(val.loginValidation);
-    const validation = schema.validate(req.body);
-
-    if (validation.error) return handleValidationError(res, validation);
-
-    next();
-  }
-
   validateUpdateContact(req, res, next) {
     const schema = Joi.object(patchValidation);
     const validation = schema.validate(req.body);
 
     if (validation.error) return handleValidationError(res, validation);
+
     next();
   }
 
@@ -62,104 +44,19 @@ class UsersController {
     }
   }
 
-  async authorize(req, res, next) {
-    try {
-      const token = req.headers.authorization.split(' ')[1];
-      let userId;
-
-      const JWT_KEY = process.env.JWT_SECRETKEY;
-
-      try {
-        userId = await jwt.verify(token, JWT_KEY).id;
-      } catch (err) {
-        console.log(err);
-      }
-      const user = await usersModel.findById(userId);
-
-      if (!user || user.token !== token)
-        return res.status(401).send('Not Authorize');
-
-      req.user = user;
-      req.token = token;
-
-      next();
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  // POST
-  registerUser = async (req, res) => {
-    try {
-      const newUser = { ...req.body };
-      const existedUser = await usersModel.findOne({
-        email: newUser.email,
-      });
-
-      if (existedUser)
-        return res.status(409).send('User with such email already exists');
-
-      const hashedPass = await bcrypt.hash(newUser.password, 10);
-      const user = await usersModel.create({ ...newUser, password: hashedPass, avatarURL: imagePath })
-      return res
-          .status(201)
-          .send({ email: user.email, password: user.password });
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  };
-
-  async loginUser(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = await usersModel.findOne({ email });
-      const JWT_KEY = process.env.JWT_SECRETKEY;
-
-      if (!user) return res.status(401).send('Email or password is wrong');
-
-      const isValidPass = await bcrypt.compare(password, user.password);
-      if (!isValidPass)
-        return res.status(401).send('Email or password is wrong');
-
-      const token = await jwt.sign({ id: user._id }, JWT_KEY, {
-        expiresIn: '20 days',
-      });
-
-      await usersModel.findByIdAndUpdate(user._id, { token });
-
-      res.status(200).send({
-        user: { email: user.email, subscription: user.subscription },
-        token,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(400).send();
-    }
-  }
-
-  async logoutUser(req, res) {
-    try {
-      const { user } = req;
-      await usersModel.findByIdAndUpdate(user._id, { token: null });
-
-      res.status(204).send();
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  }
-
   // GET
   async getCurrentUser(req, res) {
     try {
       const { user } = req;
 
       res
-          .status(200)
-          .send({ email: user.email, subscription: user.subscription });
+        .status(200)
+        .send({ email: user.email, subscription: user.subscription });
     } catch (err) {
       res.status(400).send(err.message);
     }
   }
-
+  
   // PATCH
   async updateUser(req, res) {
     try {
@@ -195,7 +92,6 @@ class UsersController {
     }
   };
 }
-
 function handleValidationError(res, val) {
   return res.status(400).send(val.error.message);
 }
